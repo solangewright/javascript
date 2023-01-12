e138a4e41df98f8c238a6ce514f76bd066e77d0c
GTA77 <solangewright@northwesternmutual.com>
2023-01-12 13:50:37 -0500

    final changes

--- projects/color-generator/script.js
+++ projects/color-generator/script.js
@@ -2,13 +2,14 @@
 const button = document.getElementById('submit');
 const para = document.getElementById('paragraph');
 
-//Hex color variable
-let hexaDecimal = [];
+
 const arrangement = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 // Function that randomizes a character 0-F, and pushes that to the hexaDemical array
 const setColor = () => {
-
+    //Hex color variable
+    let hexaDecimal = [];
+    
     for (let i = 0; i < 6; i++){
         let randNum = Math.floor(Math.random() * 16);
 
@@ -22,10 +23,9 @@ const setColor = () => {
 }
 
 const submit = () => {
-    let newColor = setColor(); 
-    document.body.style.backgroundColor = newColor;
-    para.innerText = newColor;
-    button.removeEventListener('click', submit());
+    document.body.style.backgroundColor = setColor();
+    para.innerText = setColor(); 
 }
 
 button.addEventListener('click', submit()); 
+button.removeEventListener('click', submit());
